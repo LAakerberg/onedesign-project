52dcd884eca5a6ee9d1a8cb5454aaf24220cd34a
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2023-01-13 00:10:01 +0100

    Installation of vitejs + Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{html,js}', './src/js/**/*.js', 'index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
