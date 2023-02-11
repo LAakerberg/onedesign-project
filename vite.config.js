b34816a19bfef8f4ad824bdd6d8ad3d886131a24
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-02-11 20:45:48 +0100

    Added pages/index.html

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        pages: resolve(__dirname, 'pages/index.html'),
       },
     },
   },
